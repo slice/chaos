7a6ce71fbd6c00db874ed86be38549b307af8dca
slice <ryaneft@gmail.com>
2020-02-02 20:16:36 -0800

    Reorganize `Main`, reduce rate to 5 seconds
    Again, at this point `Main` is only used for testing the `Scraper`
    functionality. It's not configurable yet, nor does it send out its
    results.

--- src/main/scala/zone/slice/chaos/Main.scala
+++ src/main/scala/zone/slice/chaos/Main.scala
@@ -2,6 +2,7 @@ package zone.slice.chaos
 
 import discord._
 import scraper._
+import scraper.errors._
 
 import cats._
 import cats.data._
@@ -13,29 +14,33 @@ import fs2._
 object Main extends IOApp {
   def print[F[_]: Sync](text: String): F[Unit] = Sync[F].delay(println(text))
 
+  def allBuildsStream[F[_]: ConcurrentEffect: Timer](
+    scraper: Scraper[F],
+    rate: FiniteDuration
+  ): Stream[F, Stream[F, Either[ScraperError, Build]]] =
+    Stream.emits(Branch.all).map { branch =>
+      branch
+        .buildStream(scraper)
+        .metered(rate)
+        .evalTap {
+          case Left(error) =>
+            print[F](show"[error] failed to scrape $branch: $error")
+          case Right(build) =>
+            print[F](show"[info] scraped $branch: $build")
+        }
+    }
+
   def program[F[_]: ConcurrentEffect: Timer]: F[Unit] =
     Stream
       .resource(Scraper.global[F])
       .flatMap { scraper =>
-        Stream
-          .emits(Branch.all)
-          .map { branch =>
-            branch
-              .buildStream(scraper)
-              .metered(10.seconds)
-              .evalTap {
-                case Left(error) =>
-                  print[F](show"[error] failed to scrape $branch: $error")
-                case Right(build) =>
-                  print[F](show"[info] scraped $branch: $build")
-              }
-          }
+        allBuildsStream(scraper, rate = 5.seconds)
           .parJoin(Branch.all.size)
       }
       .compile
       .drain
 
   override def run(args: List[String]): IO[ExitCode] = {
-    program[IO].as(ExitCode.Success)
+    IO(println("[info] scraper started")) *> program[IO].as(ExitCode.Success)
   }
 }
