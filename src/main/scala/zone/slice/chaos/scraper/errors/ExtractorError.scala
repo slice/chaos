cad9879acb97ef62e6bf77017ad0fc40ebb3f5db
slice <ryaneft@gmail.com>
2020-02-09 12:56:00 -0800

    Remove stack traces from all exceptions

--- src/main/scala/zone/slice/chaos/scraper/errors/ExtractorError.scala
+++ src/main/scala/zone/slice/chaos/scraper/errors/ExtractorError.scala
@@ -3,6 +3,8 @@ package scraper.errors
 
 import cats.Show
 
+import scala.util.control.NoStackTrace
+
 /**
   * An error from the extractor. Failure scenarios usually arise when certain
   * entities cannot be found in the content of an asset.
@@ -12,17 +14,17 @@ sealed trait ExtractorError extends Exception
 object ExtractorError {
 
   /** Thrown when no script tags were found. */
-  case object NoScripts extends ExtractorError {
+  case object NoScripts extends ExtractorError with NoStackTrace {
     override val getMessage: String = "No script tags were found"
   }
 
   /** Thrown when no stylesheet tags were found. */
-  case object NoStylesheets extends ExtractorError {
+  case object NoStylesheets extends ExtractorError with NoStackTrace {
     override val getMessage: String = "No stylesheet tags were found"
   }
 
   /** Thrown when no build number could be found. */
-  case object NoBuildNumber extends ExtractorError {
+  case object NoBuildNumber extends ExtractorError with NoStackTrace {
     override val getMessage: String = "A build number couldn't be found"
   }
 
--- src/main/scala/zone/slice/chaos/scraper/errors/ScraperError.scala
+++ src/main/scala/zone/slice/chaos/scraper/errors/ScraperError.scala
@@ -3,6 +3,8 @@ package scraper.errors
 
 import cats.Show
 
+import scala.util.control.NoStackTrace
+
 /**
   * An error from the scraper, which handles both downloading and extracting.
   */
@@ -11,12 +13,16 @@ sealed trait ScraperError extends Exception
 object ScraperError {
 
   /** A [[DownloadError]]. */
-  final case class Download(error: DownloadError) extends ScraperError {
+  final case class Download(error: DownloadError)
+      extends ScraperError
+      with NoStackTrace {
     override def getMessage: String = s"Downloader error: ${error.getMessage}"
   }
 
   /** An [[ExtractorError]]. */
-  final case class Extractor(error: ExtractorError) extends ScraperError {
+  final case class Extractor(error: ExtractorError)
+      extends ScraperError
+      with NoStackTrace {
     override def getMessage: String = s"Extractor error: ${error.getMessage}"
   }
 
