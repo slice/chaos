12ed6f869f305cf9869f5da841f9c1b44a2b6cad
slice <ryaneft@gmail.com>
2020-05-07 02:14:39 -0700

    Remove unused config fields

--- src/main/scala/zone/slice/chaos/poller/config.scala
+++ src/main/scala/zone/slice/chaos/poller/config.scala
@@ -39,9 +39,6 @@ final case class DiscordPublisherSetting(
 /** A configuration for a [[Poller]]. */
 case class Config(
     interval: FiniteDuration = 1.minute,
-    scrapeRatelimit: FiniteDuration = 5.second,
-    publishQueueChunkSize: Int = 2,
-    publishQueueInterval: FiniteDuration = 3.second,
     publishers: List[PublisherSetting] = List(),
     stateFilePath: String = "./state.chaos",
 )
